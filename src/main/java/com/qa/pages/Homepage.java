ce296fa269266a2bdc268720e3cbec3b0cfe32a3
PIRUMAL <pirumohanmalmaasitala@gmail.com>
2021-07-04 09:24:31 +0530

    demo

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class Homepage {
+
+	public void Homepage() {
+		 System.out.println("homepage");
+	}
+}
---
+++ src/test/java/com/qa/test/Loginpage.java
@@ -0,0 +1,11 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Loginpage {
+@Test
+public void test()
+{
+System.out.println("Login page");
+}
+}
